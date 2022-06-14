b6b9ba95713fe2dc1a3b9aebccff785bcf1a3edf
v.obuhov <v.obuhov@sitesoft.ru>
2022-06-14 13:25:48 +0500

    deploy

--- dist/server/src/game/server.js
+++ dist/server/src/game/server.js
@@ -2,24 +2,29 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.GameServer = void 0;
 const socket_io_1 = require("socket.io");
-const http = require("http");
+const https = require("https");
 const config_1 = require("../config");
 const manager_1 = require("../game/manager");
 const client_1 = require("../game/client");
+const fs_1 = require("fs");
 class GameServer {
     constructor() {
         this.gameManager = new manager_1.GameManager();
-        this.httpServer = http.createServer();
+        this.httpsServer = https.createServer({
+            "key": (0, fs_1.readFileSync)(config_1.Config.ssl.keyFile),
+            "cert": (0, fs_1.readFileSync)(config_1.Config.ssl.certFile),
+        });
         this.createSocketServer();
         this.bindSocketServerEvents();
         const port = config_1.Config.sockets.port;
         console.log(`Server listening on port ${port}...`);
-        this.httpServer.listen(port);
+        this.httpsServer.listen(port);
     }
     createSocketServer() {
-        this.socketServer = new socket_io_1.Server(this.httpServer, {
+        this.socketServer = new socket_io_1.Server(this.httpsServer, {
             cors: {
                 origin: config_1.Config.sockets.corsOrigin,
+                methods: ['GET', 'POST', 'OPTIONS'],
             }
         });
     }
